2010387634a1c1f14b0b9cc4f20fde6093605676
Juli <globaljuli@gmail.com>
2022-11-07 00:27:47 +0100

    product (purchase) use now returns product object

--- app/Models/ProductUse.php
+++ app/Models/ProductUse.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class ProductUse extends Model
 {
@@ -11,12 +13,15 @@ class ProductUse extends Model
 
     protected $fillable = ['purchase_id', 'use_type_id'];
 
-    protected $appends = ['purchase_name'];
+    protected $appends = ['product'];
     
-    public function getPurchaseNameAttribute(){
-        if($this->purchase_id == 1){
-            return "Nidias Xampú Sòlid - Batabat";
-        }
-        return "The Singular Olivia - Batabat";
+    /**
+     * Get product
+     */
+    public function getProductAttribute()
+    {
+        $purchase = Purchase::find($this->purchase_id);
+        $product = Product::find($purchase->product_id);
+        return $product;
     }
 }
